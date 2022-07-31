4b7b7dccfe174d87681d5b219bd8991efd33e4e8
Dukwhan Kim <ddukki@gmail.com>
2022-07-30 22:21:46 -0500

    Add Tobinian language

--- internal/entity/character/character_test.go
+++ internal/entity/character/character_test.go
@@ -3,9 +3,14 @@ package character
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNewRandomCharacter(t *testing.T) {
 	c := NewRandomCharacter()
 	fmt.Print(c.String())
+
+	assert.NotNil(t, c.baseStats)
+	assert.NotNil(t, c.education)
 }
